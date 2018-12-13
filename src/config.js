49b5eda7c2cd3a46dd89e125d6d168aa84c8f3a1
Luiz Fernando de Lima Pereira <luiz.pereira@minutrade.com>
2018-12-13 09:32:53 -0200

    adding nconf

--- src/config.js
+++ src/config.js
@@ -1,11 +1,12 @@
 const nconf = require('nconf');
 const ROOT_PATH = process.cwd();
+const path  = require("path");
 
 module.exports = nconf.argv()
 .env()
 .defaults({
-  "routeDir": "./events",
-  "handlerDir": "./handlers",
+  "routeDir": path.join(process.cwd(), "src/events"),
+  "handlerDir": path.join(process.cwd(), "src/handlers"),
   "PORT": 3000,
   "corsOptions": {
     "allowedOrigins": "*",
